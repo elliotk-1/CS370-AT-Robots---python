d902856e3822d2bb452bf0b6ea334465e246f6a0
kmulligan11 <mulligk@sunypoly.edu>
2022-10-05 14:40:48 -0400

    changes

--- background.py
+++ background.py
@@ -6,26 +6,71 @@ from pygame.locals import *
 
 pygame.init()
 
+time = 60
 width = 800
 height = 600
-window = pygame.display.set_mode((width,height))
+bg_width = 2560
+bg_height = 1600
+screen = pygame.display.set_mode((width,height))
 bg_img = pygame.image.load('background.png')
-bg_img = pygame.transform.scale(bg_img,(width,height))
+#bg_img = pygame.transform.scale(bg_img,(width,height))
 
-i = 0
+#screen_size = screen.get_size()
+#bg_size = bg_img.get_size()
+#bg_x = (bg_size[0]-screen_size[0]) // 2
+#bg_y = (bg_size[1]-screen_size[1]) // 2
+x = 0
+y = 0
 
-running = True
+clock = pygame.time.Clock()
 
-while running:
-        window.fill((0,0,0))
-        window.blit(bg_img,(i,0))
-        window.blit(bg_img,(width+i,0))
-        if(i==-width):
-                window.blit(bg_img,(width+i,0))
-                i=0
-        i-=0.05
+while True:
+        clock.tick(time)
         for event in pygame.event.get():
-                if event.type == QUIT:
-                        running = False
-        pygame.display.update()
+                if event.type == pygame.QUIT:
+                        exit()
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+                x -= 5
+        if keys[pygame.K_RIGHT]:
+                x += 5
+        if keys[pygame.K_UP]:
+                y -= 5
+        if keys[pygame.K_DOWN]:
+                y += 5
+        #bg_x = max(0, min(bg_size[0]-screen_size[0], bg_x))
+        #bg_y = max(0, min(bg_size[1]-screen_size[1], bg_y))
+        screen.blit(bg_img, (-x, -y))
+        if(x==-bg_width):
+                screen.blit(bg_img, (bg_width+x,y))
+                x=0
+        if(x==bg_width):
+                screen.blit(bg_img, (-bg_width+x,y))
+                x=0
+        if(y==-bg_height):
+                screen.blit(bg_img,(x,bg_height+y))
+                y=0
+        if(y==bg_height):
+                screen.blit(bg_img, (x, -bg_height+y))
+                y=0
+
+        pygame.display.flip()
+
+#i = 0
+
+#running = True
+
+#while running:
+        #window.fill((0,0,0))
+        #window.blit(bg_img,(i,0))
+        #window.blit(bg_img,(width+i,0))
+        #if(i==-width):
+                #window.blit(bg_img,(width+i,0))
+                #i=0
+        #i-=0.05
+        #for event in pygame.event.get():
+                #if event.type == QUIT:
+                        #running = False
+        #pygame.display.update()
 pygame.quit()
